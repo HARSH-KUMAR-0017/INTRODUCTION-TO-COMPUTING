5b428f9a3205670b3c64d1b51b2284acaf2f989c
HARSH-KUMAR-0017 <97087907+HARSH-KUMAR-0017@users.noreply.github.com>
2022-01-06 15:49:33 +0530

    Add files via upload

---
+++ ASIGNMENT 1/Asgn_1_21105022.py
@@ -0,0 +1,75 @@
+#Assignment 1
+
+
+
+                 #1st program
+print("                   1st program")
+#python program to find the avergage of the three numbers
+num_1=float(input("enter the first number ="))
+num_2=float(input("enter the second number ="))
+num_3=float(input("enter the third number ="))
+sum=num_1+num_2+num_3
+avg=sum/3
+print("the average of the numbers entered by you are=",avg)
+
+
+               #2nd program
+print("                     2nd program")
+#program to find the a person's income tax
+print('\n',"all tax payers are charged a flat tax rate of 20%",'\n',"all tax payers are allowed a $10000 standard deduction",'\n',"for each dependen, a taxpayer is allowed an additional $3000 deduction")
+Gross_income=float(input("enter the gross income to the nearest penny ="))
+std_ded=10000
+dep=int(input("enter the number of dependents ="))
+dep_ded=3000
+taxable_income=Gross_income-std_ded-(dep_ded*dep)
+print("Taxable income =$",taxable_income)
+tax=(taxable_income*20)/100
+print("the tax to be paid by the person is = $",tax)
+
+
+
+
+
+
+            #3rd prograkm
+print("                     3rd program")
+#program to store different data types values into a list
+sid=int(input("enter the sid="))
+name=input("enter the name=")
+gender=input("enter the gender F/M=")
+branch=input("enter the branch name=")
+cgpa=float(input("enter the cgpa="))
+st=[sid,name,gender,branch,cgpa]
+print(st)
+
+
+
+
+
+             #4th program
+print("                      4th program")
+#program to enter the marks of 5 students
+m1=float(input("enter the marks of 1st student="))
+m2=float(input("enter the marks of 2nd student="))
+m3=float(input("enter the marks of 3rd student="))
+m4=float(input("enter the marks of 4th student="))       
+m5=float(input("enter the marks of 5th student="))
+marks=[m1,m2,m3,m4,m5]
+marks.sort()
+print(marks)
+
+
+
+
+             #5th program
+print("                       5th program")
+#5th question of assignment
+color=['RED','GREEN','WHITE','BLACK','PINK','YELLOW']
+COLOR1=[]
+COLOR1.extend(color)
+print("ORIGINAL LIST:",COLOR1)
+COLOR1.pop(3)
+print("LIST AFTER REMOVING 4TH ELEMENT:",COLOR1)
+color[3:5]=['PURPLE']
+print("LIST AFTER REPLACING BLACK AND PINK BY PURPLE:",color)
+
---
+++ ASIGNMENT 1/Asgn_1_output_21105022.py
@@ -0,0 +1,36 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+===================== RESTART: E:\python\Asgn_1_21105022.py ====================
+                   1st program
+enter the first number =62.5
+enter the second number =32.6
+enter the third number =562.3
+the average of the numbers entered by you are= 219.13333333333333
+                     2nd program
+
+ all tax payers are charged a flat tax rate of 20% 
+ all tax payers are allowed a $10000 standard deduction 
+ for each dependen, a taxpayer is allowed an additional $3000 deduction
+enter the gross income to the nearest penny =566243
+enter the number of dependents =3
+Taxable income =$ 547243.0
+the tax to be paid by the person is = $ 109448.6
+                     3rd program
+enter the sid=21105022
+enter the name=HARSH KUMAR
+enter the gender F/M=M
+enter the branch name=ECE
+enter the cgpa=10
+[21105022, 'HARSH KUMAR', 'M', 'ECE', 10.0]
+                      4th program
+enter the marks of 1st student=56
+enter the marks of 2nd student=89
+enter the marks of 3rd student=56
+enter the marks of 4th student=98
+enter the marks of 5th student=26
+[26.0, 56.0, 56.0, 89.0, 98.0]
+                       5th program
+ORIGINAL LIST: ['RED', 'GREEN', 'WHITE', 'BLACK', 'PINK', 'YELLOW']
+LIST AFTER REMOVING 4TH ELEMENT: ['RED', 'GREEN', 'WHITE', 'PINK', 'YELLOW']
+LIST AFTER REPLACING BLACK AND PINK BY PURPLE: ['RED', 'GREEN', 'WHITE', 'PURPLE', 'YELLOW']
